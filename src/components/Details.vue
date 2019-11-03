abda4e3a23fe1ee6a4dcdc48b7f5f1eaf1285c73
Napermial <h>
2019-11-03 18:20:51 +0100

    detail page

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="Details">
+        <h1>best videos - no mum said I can have my own page</h1>
+        <div>
+            <h2 style="float: left">{{elem.id}}</h2>
+            <h2 style="float: right">{{elem.name}}</h2>
+            <iframe width="1280"
+                    height="725"
+                    :src="elem.url"
+                    allow="accelerometer;
+                    encrypted-media;
+                    gyroscope;
+                    picture-in-picture"
+                    allowfullscreen>
+            </iframe>
+        </div>
+        <h3 v-for="comment in elem.recommendation" v-bind:key="comment.id"></h3>
 
     </div>
 </template>
@@ -10,12 +25,15 @@
         name: 'Details',
         data() {
             return {
-                list:null
+                elem:null
             }
         },
-        mounted() {
-            fetch('http://localhost:8762/video',{'id':this.route.path} )
-                .then(rep => this.list = rep)
+        beforeMount() {
+            fetch('http://localhost:8762/video?id=' + this.$router.currentRoute.path.replace('/',''))
+                .then(rep => rep.json())
+                .then(rep => this.$data.elem = rep)
+                // eslint-disable-next-line no-console
+                .then(rep => console.log(rep))
         }
     }
 </script>
